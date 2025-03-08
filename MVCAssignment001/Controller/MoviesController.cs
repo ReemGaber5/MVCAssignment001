1822061c1b000b03de7fa6def92b587fd1c13e27
reemgaber213@gmail.com <reemgaber213@gmail.com>
2025-03-08 12:11:07 +0200

    Add project files.

---
+++ MVCAssignment001/Controller/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MVCAssignment001.Controller
+{
+    public class MoviesController : ControllerBase
+    {
+        //public string Index()
+        //{
+        //    return("Hello");
+        //}
+
+        #region Action Return types
+        //1-Content result
+        public ContentResult Index()
+        {
+            ContentResult result = new();
+            result.Content = "Test";
+            result.ContentType = "object/pdf";//or Text/html open html page 
+            return result;
+        }
+        #endregion
+
+
+        //public void GetMovie()
+        //{
+
+        //}
+
+    }
+}
